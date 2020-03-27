7afc785fc92bf455152e73a707f2e46ebb352d05
EgorovM <EgorovM@bitbucket.org>
2020-03-27 15:45:23 +0900

    Updates

---
+++ precache-manifest.36f6d3ad8d42bded7a6c21c004c4be44.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b1cf6105bb819833da74284451bac6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb63a511ef6a7b89c20b",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "86d44d952b68b78d25bd",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "fb63a511ef6a7b89c20b",
+    "url": "/static/js/2.c1e85cdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1e85cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86d44d952b68b78d25bd",
+    "url": "/static/js/main.e41433f8.chunk.js"
+  },
+  {
+    "revision": "e73130f90b72e6a86f82",
+    "url": "/static/js/runtime-main.c653f2a1.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
